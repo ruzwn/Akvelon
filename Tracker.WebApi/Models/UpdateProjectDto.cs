c5e4c85fef34a142f1506cecaf09806557c1a828
ruzwn <ruzwn@users.noreply.github.com>
2021-12-31 01:06:33 +0300

    Add some Required attribute

--- Tracker.WebApi/Models/UpdateProjectDto.cs
+++ Tracker.WebApi/Models/UpdateProjectDto.cs
@@ -6,9 +6,11 @@ namespace Tracker.WebApi.Models
 {
 	public class UpdateProjectDto
 	{
-		public            Guid   Id       { get; set; }
-		[Required] public string Name     { get; set; } 
-		public ProjectStatus Status { get; set; }
-		public            int    Priority { get; set; }
+		[Required]
+		public Guid Id { get; set; }
+		[Required]
+		public string Name { get;            set; }
+		public ProjectStatus Status   { get; set; }
+		public int           Priority { get; set; }
 	}
 }
--- Tracker.WebApi/Models/UpdateTaskDto.cs
+++ Tracker.WebApi/Models/UpdateTaskDto.cs
@@ -6,11 +6,14 @@ namespace Tracker.WebApi.Models
 {
 	public class UpdateTaskDto
 	{
-		public            Guid         ProjectId   { get; set; }
-		public            Guid         Id          { get; set; }
-		[Required] public string       Name        { get; set; }
-		public            string       Description { get; set; }
-		public            MyTaskStatus Status      { get; set; }
-		public            int          Priority    { get; set; }
+		[Required]
+		public Guid ProjectId { get; set; }
+		[Required]
+		public Guid Id        { get; set; }
+		[Required]
+		public string Name { get;              set; }
+		public string       Description { get; set; }
+		public MyTaskStatus Status      { get; set; }
+		public int          Priority    { get; set; }
 	}
 }
